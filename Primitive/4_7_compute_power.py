ed51f2a3db32e018ed052b0222b144c3ad5ce9ad
Pablo Dominguez <pablo.mdd.007@gmail.com>
2020-11-28 18:49:45 -0600

    add 4_7 compute power

---
+++ Primitive/4_7_compute_power.py
@@ -0,0 +1,21 @@
+
+def power(x: float, y: int) -> float:
+    result = 1.0
+    power = y
+
+    # tweek for the algorithm work with negative power
+    if y < 0:
+        power, x = -power, 1.0 / x
+
+    # if power < 0 evaluates False
+    # that happens when bits shift down to 0 or less
+    while power:
+        # evaluates if power is odd
+        if power & 1:
+            # If odd, multiply per x to not loose that factor in interger division
+            result *= x
+        # right bit shift -> divides by 2
+        power = power >> 1
+        # Accumulate power
+        x = x * x
+    return result   
